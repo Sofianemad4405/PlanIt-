24946d7392a05de211b7f7a9df4eee4e838abadf
Sofiian <sofianemad98@gmail.com>
2025-08-14 14:54:57 +0300

    thirs commit

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
+      // _counter without callisssssssssng setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
     });
@@ -114,7 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Incrementsssss',
+        tooltip: 'Incrementssssssssssss',
         child: GestureDetector(child: const Icon(Icons.add)),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
