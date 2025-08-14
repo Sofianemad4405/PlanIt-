be2c9f91457f4fdd5645558ec7b74afc624b6fce
Sofiian <sofianemad98@gmail.com>
2025-08-14 14:52:07 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -114,8 +114,8 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        tooltip: 'Incrementsssss',
+        child: GestureDetector(child: const Icon(Icons.add)),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
